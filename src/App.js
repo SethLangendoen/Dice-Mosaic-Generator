bc1ba2395741dfda94c2ef1d72b65d1d5af79278
slangend <=>
2024-10-19 03:27:04 -0400

    analytics bug fix 2

--- src/App.js
+++ src/App.js
@@ -1,36 +1,60 @@
 import './App.css';
 import DiceArtHomepage from './components/DiceArtHomepage';
-import './components/styling.css'
+import './components/styling.css';
 import ApolloClientProvider from './components/ApolloClientProvider';
-import { CartProvider } from './components/context/CartContext' // Import CartProvider
+import { CartProvider } from './components/context/CartContext';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import BlogComponent from './components/blog/blogComponent';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import MosaicShop from './components/MosaicShop';
-
+import { useEffect } from 'react';
 
 function App() {
+  useEffect(() => {
+    // Ensure the document head exists and then load the Google Analytics script
+    const loadGoogleAnalytics = () => {
+      if (typeof document !== 'undefined' && document.head) {
+        // Create the script element
+        const script = document.createElement('script');
+        script.async = true;
+        script.src = 'https://www.googletagmanager.com/gtag/js?id=G-B3MKBFTVXS';
+
+        // Add the script to the document head
+        document.head.appendChild(script);
+
+        // Initialize Google Analytics
+        window.dataLayer = window.dataLayer || [];
+        function gtag() {
+          window.dataLayer.push(arguments);
+        }
+        gtag('js', new Date());
+        gtag('config', 'G-B3MKBFTVXS');
+      }
+    };
+
+    // Call the function to load Google Analytics
+    loadGoogleAnalytics();
+  }, []); // Empty array ensures this effect runs only once
 
   return (
     <div className="App">
-    <Router>
-    <Navbar />
-    <ApolloClientProvider >
-      <CartProvider>
-        <Routes>
-            <Route path="/" element={<DiceArtHomepage /> }/>
-            <Route path="/blog" element={<BlogComponent />} />
-            <Route path="/shop" element={<MosaicShop />} />
+        <title>Dice Art Generator</title>
 
-
-        </Routes>
-      </CartProvider>
-    </ApolloClientProvider>
-    </Router>
+      <Router>
+        <Navbar />
+        <ApolloClientProvider>
+          <CartProvider>
+            <Routes>
+              <Route path="/" element={<DiceArtHomepage />} />
+              <Route path="/blog" element={<BlogComponent />} />
+              <Route path="/shop" element={<MosaicShop />} />
+            </Routes>
+          </CartProvider>
+        </ApolloClientProvider>
+      </Router>
     </div>
-
   );
 }
 
